a4b5cfb842d80df669ee156854a2a601e6c53193
leticia <leticia.nuness.santos@gmail.com>
2024-01-27 21:49:29 +0000

    combined code leticia and bek final version

--- projectile.py
+++ projectile.py
@@ -1,26 +1,10 @@
+# Practice with complex maths
 from math import pi, tan, cos
+# import math - math.pi
 
-
-# Practice with complex maths
-
-# gravity = pow(9.81, 2)
-# initial_vorticity = 44
-# theta = 80 * (pi/80)
-# distance = 0.5
-# height = 1
-# x = pi
-
-y = 1 + 0.5*tan(80*(pi/180)) - pow(9.81, 2) / 2*pow(44*cos(80*(pi/180)), 2)
-print(y)
-
-
-# TO DO: Method and Modules
-# Practice with complex maths
-
-# import the math module to be able to use pi, tan (tangent) and cos (cosine) in calculation
-import math
 # print math.pi to check pi is working - shows 3.141592653589793
-print(f"pi = {math.pi}")
+# print(f"pi = {math.pi}")
+print(f"pi = {pi}")
 
 # declare the variables as per the instructions given above:
 # declaring the height of the barrel (y0) as 1 meter
@@ -31,7 +15,8 @@ x = 0.5
 
 # theta = deg * (pi/180) from instructions =
 # theta = 80 * (3.141592653589793 / 180)
-theta = 80 * (math.pi / 180)
+# theta = 80 * (math.pi / 180)
+theta = 80 * (pi / 180)
 # printing the value of theta determined by the above calculation
 print(f"theta = {theta}")
 
@@ -42,7 +27,10 @@ g = 9.81
 v0 = 44
 
 # the equation written in python (I hope this is correct!)
-y = y0 + x * math.tan(theta) - ((g * x**2) / (2 * (v0 * math.cos(theta)))**2)
+# y = y0 + x * math.tan(theta) - ((g * x**2) / (2 * (v0 * math.cos(theta)))**2)
+
+y = y0 + x * tan(theta) - ((g * x**2) / (2 * (v0 * cos(theta)))**2)
+
 # math.tan() function returns the tangent of value passed as argument
 # math.cos() function returns the cosine of value passed as argument
 
@@ -54,6 +42,11 @@ y_rounded2 = round(y, 2)
 
 # print y rounded to 2dp, which is the outcome of the calculation
 print(f"the height of the projectile is {y_rounded2} meters")
+
+# hard coded the values - to make of my code and thinking steps 
+y = 1 + 0.5*tan(80*(pi/180)) - pow(9.81, 2) / 2*pow(44*cos(80*(pi/180)), 2)
+
+print(y)
 
 # //////// ORIGINAL QUESTION ////////
 
--- types_practice.py
+++ types_practice.py
@@ -1,117 +1,58 @@
 # Practice declaring variables and using different types
-# 1. Why is this coding concept this way?
-# 2. Is there another way to do this?
-# 3. What can I build to help me learn this more? (Maybe not want to build with it)
-from typing import List
-# create two variables: one containing your first name and another containing your last name
-# created two variables: first_name and last_name
+
+
+# EXERCISE: create two variables: one containing your first name and another containing your last name
 # Assigned a string value to each variable ''
 # variables are containers for storing data value
+# give meaningful names to both variables using snakecase
 first_name = 'Leticia'
 last_name = 'Santos'
 
-# use the print function to output your full name with a space between your first and last name
+# summary: the print function is used to run the arguments of the parameters inside the parentheses.
 print(first_name, last_name)
 
-# notes on above block of code
-# summary: the print function is used to run the arguments of the parameters inside the parentheses.
-# function is a block of code which runs when called.
-# parentheses () are necessary to call on a function - allow the code it to run.
-# parameters is the variables listed inside the parentheses ()
-# arguments is the values sent to the function when it is called
-# print is a function which runs the string values assigned to the variables inside the brackets
-# This displays the first and last name with a space
+# concatenate the two variables with + " " +
+print(first_name + " " + last_name)
 
+# concatenate using .format() method - better than previous version
+print("{} {}".format(first_name, last_name))
 
-# transfer these variable values into a list
-# look into rules of a list and explain
-# lists are ordered so will display the first value in the list
-# created a list [] to transfer variable values first_name and last_name
-# Concatenated with + both variable to list both variable values together with a name
-full_name_list = ['leticia' + ' ' + 'santos', ]
-print(type(full_name_list))
-print(full_name_list)
-# is there a way to use the print function to display the list values and store the names without concantenation
-# print(full_name_list[0:]) prints first name and excludes last
+# use an f-string to concatenate the variables - best way as easily read
+print(f"{first_name} {last_name}")
 
-# TO DO: add to list, remove list, extract and put into an f-string
-# list slicing
+# EXERCISE - transfer these variable values into a list
 
-full_name_1 = ['leticia', 'santos']
-print(full_name_1)
+# lists full_name concatenate the two list items as one with + " " +
+# created two lists and input string items in the []
+full_name = ['leticia' + ' ' + 'santos', ]  # think it is bad coding,
+full_name1 = ['leticia', 'santos']
 
-# take the variables and now store the values in a dictionary, using the keys 'first' and 'last'
-# key: values
+# summary: the print function is used to run the list items of the list variable names above inside the parentheses.
+# print the new variable
+print(full_name)
+print(full_name, full_name1)
+print(type(full_name))
+
+# Lists are mutable, therefore values can change
+# index 0 = leticia and index 1 = santos
+# if I were to get married I would change the string value of index 1
+full_name1[1] = "Odigili"
+print(full_name1)
+
+# EXERCISE: take the variables and now store the values in a dictionary, using the keys 'first' and 'last'
+# create a new variable for the dictionary name eg. name
+# use curly braces for a dictionary
+# Declare as follows; '"key": value' and separate with a comma
 
 full_name_dictionary = {
     'first': 'Leticia',
     'last': 'Santos'
 }
 
-# display the dictionary values
+# print the dictionary values
 print(type(full_name_dictionary))
 print(full_name_dictionary)
-# EXERCISE - create two variables: one containing your first name and another containing your last name
-# give meaningful names to both variables using snakecase
-first_name = "Bek"
-last_name = "Rootes"
-
-# EXERCISE - use the print function to output your full name with a space between your first and last name
-# concatenate the two variables with + " " +
-print(first_name + " " + last_name)
-
-# concatenate using .format() method - better than previous version
-print(("{} {}").format(first_name, last_name))
-
-# use an f-string to concatenate the variables - best way as easily read
-print(f"{first_name} {last_name}")
-# all print as: Bek Rootes
-
-# EXERCISE - transfer these variable values into a list
-# create a new variable for the list name and enter the existing variables within square brackets (list),
-# separated by a comma
-full_name = [first_name, last_name]
-# print the new variable
-print(full_name)
-# prints as: ['Bek', 'Rootes']
-
-# Lists are mutable, therefore values can change
-# index 0 = first_name and index 1 = last_name
-# if I were to get married I would change the string value of index 1
-full_name[1] = "Glean"
-print(full_name)
-
-# TO DO: add to list, remove from list, extract and put into an f-string
-# list slicing
-
-# EXERCISE - take the variables and now store the values in a dictionary, using the keys 'first' and 'last'
-# create a new variable for the dictionary name eg. name
-# use curly braces for a dictionary
-# Declare as follows; '"key": value' and separate with a comma
-name = {
-    "first": first_name,
-    "last": last_name
-}
-
-# or
-
-name2 = {
-    "first": "Bek",
-    "last": "Rootes"
-}
-
-# EXERCISE - display the dictionary values
-# print the dictionary variable
-print(name)
-print(name2)
-# prints as: {'first": 'Bek', 'last': 'Rootes'}
-
-# //////// EXTRAS ////////
 
 # Access the value of keys
-print(name["first"])
-# prints as: Bek
-print(name["last"])
-# prints as: Rootes
-
-# TO DO: print type!
+print(full_name_dictionary["first"])
+print(full_name_dictionary["last"])
